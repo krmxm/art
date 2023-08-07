ad3b43c143ec3acd44f9899584d4661215ef1a5b
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-08-08 01:51:08 +0300

    sliders and forms secondary

---
+++ src/js/modules/forms.js
@@ -0,0 +1,85 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+
+    // checkNumInputs('input[name="user_phone"]');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Скоро мы с вами свяжемся!',
+        failure: 'Ошибка',
+        spinner: 'assets/img/spinner.gif',
+        ok: 'assets/img/ok.png',
+        fail: 'assets/img/fail.png'
+    };
+
+    const path = {
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
+    };
+
+    const postData = async (url, data) => {
+        
+        const res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+ 
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.parentNode.appendChild(statusMessage);
+
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400);
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loading;
+            statusMessage.appendChild(textMessage);
+
+            const formData = new FormData(item);
+            let api;
+            item.closest('.popup-design') ? api = path.designer : api = path.designer;
+            console.log(api);    
+
+            postData(api, formData)
+                .then(res => {
+                    console.log(res);
+                    statusImg.setAttribute('src', message.ok);
+                    textMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusImg.setAttribute('src', message.fail);
+                    textMessage.textContent = message.failure;
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
