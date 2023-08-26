0de7f019de5498dc93d30e1e6e031db80355f659
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-08-26 23:41:51 +0300

    filter

--- dist/script.js
+++ dist/script.js
@@ -151,52 +151,40 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const filter = () => {
   const menu = document.querySelector('.portfolio-menu'),
-    items = menu.querySelectorAll('li'),
-    btnAll = menu.querySelector('.all'),
-    btnLovers = menu.querySelector('.lovers'),
-    btnChef = menu.querySelector('.chef'),
-    btnGirl = menu.querySelector('.girl'),
-    btnGuy = menu.querySelector('.guy'),
-    btnGrandmother = menu.querySelector('.grandmother'),
-    btnGranddad = menu.querySelector('.granddad'),
+    items = document.querySelectorAll('li'),
     wrapper = document.querySelector('.portfolio-wrapper'),
-    markAll = wrapper.querySelectorAll('.all'),
-    markGirl = wrapper.querySelectorAll('.girl'),
-    markLovers = wrapper.querySelectorAll('.lovers'),
-    markChef = wrapper.querySelectorAll('.chef'),
-    markGuy = wrapper.querySelectorAll('.guy'),
+    allMark = wrapper.querySelectorAll('.all'),
     no = document.querySelector('.portfolio-no');
-  const typeFilter = markType => {
-    markAll.forEach(mark => {
-      mark.style.display = 'none';
-      mark.classList.remove('animated', 'fadeIn');
-      no.style.display = 'none';
-      no.classList.remove('animated', 'fadeIn');
-      if (markType) {
-        markType.forEach(mark => {
-          mark.style.display = 'block';
-          mark.classList.add('animated', 'fadeIn');
-        });
-      } else {
-        no.style.display = 'block';
-        no.classList.add('animated', 'fadeIn');
-      }
+  function typeFilter(mark) {
+    allMark.forEach(item => {
+      item.style.display = 'none';
+      item.classList.remove('animated', 'fadeIn');
     });
-  };
-  btnAll.addEventListener('click', () => {
-    typeFilter(markAll);
-  });
-  btnLovers.addEventListener('click', () => {
-    typeFilter(markLovers);
-  });
+    no.style.display = 'none';
+    no.classList.remove('animated', 'fadeIn');
+    if (mark) {
+      mark.forEach(item => {
+        item.style.display = 'block';
+        item.classList.add('animated', 'fadeIn');
+      });
+    }
+    if (mark.length < 1) {
+      no.style.display = 'block';
+      no.classList.add('animated', 'fadeIn');
+    }
+    console.log(mark);
+  }
   menu.addEventListener('click', e => {
     const target = e.target;
-    if (target && target.tagName == "LI") {
+    if (target && target.tagName === "LI") {
       items.forEach(item => {
         item.classList.remove('active');
       });
       target.classList.add('active');
     }
+    const selectClass = target.classList[0],
+      showElems = wrapper.querySelectorAll(`.${selectClass}`);
+    typeFilter(showElems);
   });
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (filter);
